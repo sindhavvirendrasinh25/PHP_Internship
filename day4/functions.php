f28d0fdc8fd4debb355cf8f3791a51d0e5224097
Virendra Sindhav <sindhavvirendrasinh25@gmail.com>
2026-05-15 11:00:26 +0530

    Completed day4 task1

---
+++ day4/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+// Function with Parameters and Return Value
+function addNumbers($a, $b)
+{
+    return $a + $b;
+}
+
+$result = addNumbers(10, 20);
+
+echo "<h2>Function with Parameters and Return Value</h2>";
+echo "Addition = " . $result . "<br><br>";
+
+
+// Built-in String Functions
+$str = "Virendrasinh";
+
+echo "<h2>Built-in String Functions</h2>";
+
+echo "Original String: " . $str . "<br>";
+echo "Length: " . strlen($str) . "<br>";
+echo "Uppercase: " . strtoupper($str) . "<br>";
+echo "Lowercase: " . strtolower($str) . "<br>";
+echo "Replace PHP with World: " . str_replace("PHP", "World", $str) . "<br>";
+echo "Substring: " . substr($str, 0, 5) . "<br><br>";
+
+
+// Built-in Math Functions
+$num = 25.7;
+
+echo "<h2>Built-in Math Functions</h2>";
+
+echo "Original Number: " . $num . "<br>";
+echo "Absolute Value: " . abs($num) . "<br>";
+echo "Square Root of 25: " . sqrt(25) . "<br>";
+echo "2 Power 3: " . pow(2, 3) . "<br>";
+echo "Rounded Value: " . round($num) . "<br>";
+echo "Random Number: " . rand(1, 100) . "<br>";
+
+?>
---
+++ day4/student_portal/contact.php
@@ -0,0 +1,8 @@
+<?php include_once 'header.php'; ?>
+
+<h2>Contact Us</h2>
+
+<p>Email: studentportal@gmail.com</p>
+<p>Phone: +91 9876543210</p>
+
+<?php include_once 'footer.php'; ?>
